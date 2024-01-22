a317862c3c61735084efcfb907349225e943391c
Paul Martin <paul@paulsputer.com>
2024-01-22 23:15:14 +0800

    Basic Gym dropdown component

---
+++ src/lib/GymDropdown.svelte
@@ -0,0 +1,47 @@
+<script>
+	// @ts-nocheck
+	import { page } from '$app/stores';
+	export let props;
+	export let name;
+	export let options = [];
+	export let label = name;
+</script>
+
+<div class="holder">
+	<label>
+		<span>{label ?? name}</span>
+		<select
+			bind:value={props[name]}
+			on:input={(e) => {
+				props[name] = e.target.value;
+				$page.url.searchParams.set(name, props[name]);
+			}}
+		>
+			{#each options as opt}
+				<option value={opt}>
+					{opt}
+				</option>
+			{/each}
+		</select>
+	</label>
+</div>
+
+<style>
+	.holder {
+		padding: 0 1em;
+	}
+
+	.holder label {
+		color: #000;
+	}
+
+	label span {
+		text-transform: capitalize;
+		font-weight: 600;
+		padding-top: 0.5em;
+	}
+
+	select {
+		background-color: #fff;
+	}
+</style>
