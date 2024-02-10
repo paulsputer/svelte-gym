bc5c3c8d5f999cd55a4991330fc0a71d1ce7e234
Paul Martin <paul@paulsputer.com>
2024-02-10 14:07:57 +0800

    Fixes #2 grid options (#8)
    true/false continue to operate but the following are now available:
    
      + none
      + light
      + dark
      + black
      + white
      + textBlack
      + textWhite

--- src/lib/TestHarness.svelte
+++ src/lib/TestHarness.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import 'ress';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
@@ -8,12 +8,11 @@
 	import GymCheckbox from './GymCheckbox.svelte';
 	import GymSlider from './GymSlider.svelte';
 	import GymButton from './GymButton.svelte';
+	import GymDropdown from './GymDropdown.svelte';
 
-	export let maxWidth = 500;
-	export let maxHeight = 500;
-	export let maxFontSize = 50;
-
-	let params = $page.url.searchParams;
+	export let maxWidth: number | null = null;
+	export let maxHeight: number | null = null;
+	export let maxFontSize: number | null = null;
 
 	let props = {
 		__controls: true,
@@ -24,6 +23,33 @@
 		__fontsize: '1em',
 		__resetAnimations: () => {}
 	};
+
+	const gridOptions = [
+		{ value: 'none', class: 'no-grid' },
+		{ value: 'light', class: 'grid-basic-light' },
+		{ value: 'dark', class: 'grid-basic-dark' },
+		{ value: 'black', class: 'grid-black-grid-white-bg' },
+		{ value: 'white', class: 'grid-white-grid-black-bg' },
+		{ value: 'textBlack', class: 'grid-black-text-white-bg' },
+		{ value: 'textWhite', class: 'grid-white-text-black-bg' }
+	];
+
+	function getGridClassFromValue(v) {
+		// support legacy mode was boolean
+		if (v === true || v === 'true') {
+			return getGridClassFromValue('light');
+		} else if (v === false || v === 'false') {
+			return getGridClassFromValue('none');
+		}
+
+		const g = gridOptions.filter((e) => e.value === v);
+
+		if (g.length > 0) {
+			return g[0].class;
+		}
+
+		return getGridClassFromValue('none');
+	}
 
 	onMount(() => {
 		let hasAnimations = document.getAnimations().length > 0;
@@ -47,7 +73,7 @@
 </script>
 
 <section>
-	<div class="test-grid" class:grid={props.__grid}></div>
+	<div class="test-grid {getGridClassFromValue(props.__grid)}"></div>
 
 	<div class="test-area" class:controls={props.__controls}>
 		<div class="test-holder">
@@ -70,7 +96,15 @@
 				<br />
 				<ul>
 					<li><GymCheckbox hideExtra={true} bind:props name="__controls" label="controls" /></li>
-					<li><GymCheckbox hideExtra={true} bind:props name="__grid" label="grid" /></li>
+					<GymDropdown
+						hideExtra={true}
+						bind:props
+						name="__grid"
+						label="grid"
+						options={gridOptions}
+						optLabel="value"
+						optValue="value"
+					/>
 					<li><GymCheckbox hideExtra={true} bind:props name="__highlight" label="highlight" /></li>
 				</ul>
 				<GymButton bind:props name="__resetAnimations" label="Reset Animations" />
@@ -79,7 +113,6 @@
 				<GymSlider
 					hideExtra={true}
 					units="px"
-					min={0}
 					max={maxWidth}
 					bind:props
 					name="__width"
@@ -88,7 +121,6 @@
 				<GymSlider
 					hideExtra={true}
 					units="px"
-					min={0}
 					max={maxHeight}
 					bind:props
 					name="__height"
@@ -97,7 +129,6 @@
 				<GymSlider
 					hideExtra={true}
 					units="px"
-					min={5}
 					max={maxFontSize}
 					bind:props
 					name="__fontsize"
@@ -118,10 +149,6 @@
 	section hr {
 		border-color: #999;
 	}
-	.test-grid.grid {
-		visibility: visible;
-	}
-
 	.test-controls a,
 	.test-controls a:visited {
 		color: #000;
@@ -129,37 +156,12 @@
 	}
 
 	.test-grid {
-		visibility: hidden;
 		width: 100%;
 		height: 100%;
 		position: absolute;
 		top: 0;
 		left: 0;
-		background-image: linear-gradient(to right, grey 1px, transparent 1px),
-			linear-gradient(to bottom, grey 1px, transparent 1px);
-		background-size: 20px 20px;
-		background-position:
-			0 0,
-			10px 10px;
-		background-repeat: repeat, repeat;
-		z-index: -100;
-		opacity: 0.1;
-	}
-
-	.test-grid::before,
-	.test-grid::after {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-image: linear-gradient(to right, black 1px, transparent 1px),
-			linear-gradient(to bottom, black 1px, transparent 1px);
-		background-position: 0 0;
-		background-size: 10px 10px;
-		z-index: -1;
-		opacity: 0.1;
+		z-index: -900;
 	}
 
 	.test-area {
@@ -218,5 +220,66 @@
 	ul {
 		list-style-type: none;
 		padding-left: 1em;
+	}
+
+	.grid-basic-light {
+		background:
+			linear-gradient(#eee 1px, transparent 0) repeat-y,
+			linear-gradient(90deg, #eee 1px, transparent 0) repeat-x,
+			white;
+		background-size: 20px 20px;
+		background-position:
+			0 0,
+			10px 10px;
+		background-repeat: repeat, repeat;
+	}
+
+	.grid-basic-dark {
+		background:
+			linear-gradient(#aaa 1px, transparent 0) repeat-y,
+			linear-gradient(90deg, #aaa 1px, transparent 0) repeat-x,
+			white;
+		background-size: 20px 20px;
+		background-position:
+			0 0,
+			10px 10px;
+		background-repeat: repeat, repeat;
+	}
+	.grid-black-grid-white-bg {
+		background:
+			linear-gradient(black 1px, transparent 0) repeat-y,
+			linear-gradient(90deg, black 1px, transparent 0) repeat-x,
+			white;
+		background-size: 20px 20px;
+		background-position:
+			0 0,
+			10px 10px;
+		background-repeat: repeat, repeat;
+	}
+
+	.grid-white-grid-black-bg {
+		background:
+			linear-gradient(white 1px, transparent 0) repeat-y,
+			linear-gradient(90deg, white 1px, transparent 0) repeat-x,
+			black;
+		background-size: 20px 20px;
+		background-position:
+			0 0,
+			10px 10px;
+		background-repeat: repeat, repeat;
+	}
+
+	.grid-black-text-white-bg {
+		background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="500" height="20" viewBox="0 0 45 10"><text x="0" y="8" font-size="8" fill="black" font-family="Arial">Lorem ipsum</text></svg>');
+		background-repeat: repeat;
+		background-size: 140px 30px;
+		background-color: white;
+	}
+
+	.grid-white-text-black-bg {
+		background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="500" height="20" viewBox="0 0 45 10"><text x="0" y="8" font-size="8" fill="white" font-family="Arial">Lorem ipsum</text></svg>');
+		background-repeat: repeat;
+		background-size: 140px 30px;
+		background-color: black;
 	}
 </style>
